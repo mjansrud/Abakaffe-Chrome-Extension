a6f8806537afe38c22325cac2d431f2458a79a58
Morten Jansrud <morten@founder.no>
2016-10-28 14:46:30 +0200

    Added 1.0.8 and 1.0.9 for backup

---
+++ 1.0.9/script.js
@@ -0,0 +1,91 @@
+$(function() {
+
+	var html;
+	var status;
+	var start;
+	var hours;
+	var minutes;
+    var hours_text = '';
+    var minutes_text = '';
+    var elapsed;
+
+	console.log("Script loaded");
+
+	$("#expand").click(function () {
+		if($("#help").is(":visible")){
+			$("#help").slideUp("slow");
+			$("#expand").html("Mottar du ikke varsler?");
+		}else{
+			$("#help").slideDown("slow");
+			$("#expand").html("Lukk");
+		}
+	});
+
+	$("#settings").click(function () {
+		chrome.tabs.create({url: "chrome://flags/#enable-native-notifications"});
+	});
+
+
+
+	chrome.storage.sync.get("request", function (response) {
+		
+		if(response.request == true){
+			$("#content").html('<div>Du har allerede forespurt varsling.</div><button id="abort" type="button" class="btn btn-warning">Avbryt</button>');
+			$("#abort").on("click", function(){
+				chrome.storage.sync.set({"request": false}, function() {
+					$("#content").html('<div>Du vil ikke lenger motta et varsel.</div> ');
+					_gaq.push(['_trackEvent', 'request', 'cancelled']);
+				}); 
+			});
+		}
+		if(response.request == null || response.request == false){
+			chrome.storage.sync.set({"request": true}, function() {
+				$("#content").html('Du vil motta et varsel når kaffen er klar! <button id="abort" type="button" class="btn btn-warning">Avbryt</button>');
+				$("#abort").on("click", function(){
+					chrome.storage.sync.set({"request": false}, function() {
+						$("#content").html('<div>Du vil ikke lenger motta et varsel.</div> ');
+						_gaq.push(['_trackEvent', 'request', 'cancelled']);
+					}); 
+				});
+				_gaq.push(['_trackEvent', 'request', 'saved']);
+			}); 
+		}	
+	
+		
+	});
+
+	function fetchStatus(){
+        console.log("Fetching Abacoffee status")
+		$.getJSON("https://kaffe.abakus.no/api/status", function (data) {
+            console.log("Success! Updating values.")
+			html 	= "Kaffetrakteren er";
+			status  = data["coffee"]["status"];
+			start   = data["coffee"]["last_start"];
+			hours   = data["coffee"]["time_since"]["hours"];
+			minutes = data["coffee"]["time_since"]["minutes"];
+			if(status){
+				html += " på!";
+			}else{
+				html += " av!";
+			}
+			if(hours != 0){
+				if(hours == 1){
+					hours_text =  hours + " time og ";
+				}else{
+					hours_text =  hours + " timer og ";
+				}
+			}
+            if(minutes == 1){
+                minutes_text = minutes + " minutt";
+            }else{
+				minutes_text = minutes + " minutter";
+            }
+			$("#status").html(html + "</br> Skrudd på for " + hours_text + minutes_text + " siden.");
+            $("#loading").slideUp("fast");
+            $("#status").slideDown("fast");
+        }); 
+	}
+
+    setTimeout(fetchStatus, 500);
+	
+});
